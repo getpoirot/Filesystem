9e71a0067d175e47dc352787f5a49397643b634f
Payam Naderi <naderi.payam@gmail.com>
2015-01-17 14:24:45 +0330

    make safe filename

--- Util.php
+++ Util.php
@@ -80,4 +80,18 @@ class Util
 
         return $ret;
     }
+
+    /**
+     * Generate Safe Web Name From Filename
+     *
+     * @param string $fileName
+     *
+     * @return string
+     */
+    public static function makeSafe($fileName)
+    {
+        $regex = array('#(\.){2,}#', '#[^A-Za-z0-9\.\_\-]#', '#^\.#');
+
+        return preg_replace($regex, '', $fileName);
+    }
 }
