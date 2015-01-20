2252971d58c5578349cee5fb3b4899051276d8d1
Payam Naderi <naderi.payam@gmail.com>
2015-01-20 12:26:17 +0330

    implement setup from uri

--- Ftp/FtpOptions.php
+++ Ftp/FtpOptions.php
@@ -15,8 +15,39 @@ class FtpOptions extends AbstractOptions
 
     protected $serverUri;
     protected $port = 21;
+    protected $timeout = 90;
+
     protected $username;
     protected $password;
+
+    /**
+     * Set Options From Uri
+     *
+     * @param string $uri
+     *
+     * @throws \Exception
+     * @return $this
+     */
+    function fromUri($uri)
+    {
+        // Split FTP URI into:
+        // $match[0] = ftp://username:password@sld.domain.tld/path1/path2/
+        // $match[1] = ftp://
+        // $match[2] = username
+        // $match[3] = password
+        // $match[4] = sld.domain.tld
+        // $match[5] = /path1/path2/
+        preg_match("/ftp:\/\/(.*?):(.*?)@(.*?)(\/.*)/i", $uri, $match);
+
+        $options = [];
+        $options['username']   = $match[2];
+        $options['password']   = $match[3];
+        $options['server_uri'] = $match[4];
+
+        $this->fromArray($options);
+
+        return $this;
+    }
 
     /**
      * @return mixed
@@ -30,9 +61,17 @@ class FtpOptions extends AbstractOptions
      * ! Refresh FTP Connection
      *
      * @param mixed $serverUri
+     * @throws \Exception
      */
     function setServerUri($serverUri)
     {
+        $serverUri = strtolower($serverUri);
+        if (strpos($serverUri, 'ftp://'))
+            throw new \Exception(
+                "The FTP server address shouldn't have any trailing slashes
+                and shouldn't be prefixed with ftp://
+            ");
+
         $this->serverUri = $serverUri;
 
         (!$this->ftpFilesystem) ?: $this->ftpFilesystem->refreshResource = true;
@@ -91,14 +130,29 @@ class FtpOptions extends AbstractOptions
     }
 
     /**
-     * ! If Connection Available On Belong Filesystem
-     *   on any related option changes refresh the connection
-     *   on filesystem
+     * ! If Connection has made On injected Filesystem
+     *   with any related option changes refresh the connection
      *
      * @param mixed $ftpFilesystem
      */
     function setFtpFilesystem(Filesystem $ftpFilesystem)
     {
         $this->ftpFilesystem = $ftpFilesystem;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
+    /**
+     * @param mixed $timeout
+     */
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
     }
 }
